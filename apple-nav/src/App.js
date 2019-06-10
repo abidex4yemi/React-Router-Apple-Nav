eec9b4f37e47632cbb2c08778e17f84e9ef0e7ce
abidex4yemi <abidex4yemi@gmail.com>
2019-06-10 16:57:24 +0100

    Add route data

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { NavWrapper } from './components/Navigation/NavWrapper';
 import './App.css';
 
---
+++ apple-nav/src/components/data/routeDetails.js
@@ -0,0 +1,54 @@
+import uuid from 'uuid';
+
+export const routeDetails = [
+	{
+		id: uuid(),
+		path: '/mac',
+		ComponentToRender: ''
+	},
+	{
+		id: uuid(),
+		path: '/ipad',
+		ComponentToRender: ''
+	},
+	{
+		id: uuid(),
+		path: '/iphone',
+		ComponentToRender: ''
+	},
+	{
+		id: uuid(),
+		path: '/ipad',
+		ComponentToRender: ''
+	},
+	{
+		id: uuid(),
+		path: '/watch',
+		ComponentToRender: ''
+	},
+	{
+		id: uuid(),
+		path: '/tv',
+		ComponentToRender: 'tv'
+	},
+	{
+		id: uuid(),
+		path: '/music',
+		ComponentToRender: ''
+	},
+	{
+		id: uuid(),
+		path: '/support',
+		ComponentToRender: ''
+	},
+	{
+		id: uuid(),
+		path: '/search',
+		ComponentToRender: ''
+	},
+	{
+		id: uuid(),
+		path: '/us/shop/goto/bag',
+		ComponentToRender: ''
+	}
+];
